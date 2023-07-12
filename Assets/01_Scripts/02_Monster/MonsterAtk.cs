88b8b34366ebb57d8a74bc29e258afdfe629220e
jangeungue <zzanggu2930@gmail.com>
2023-07-12 20:02:22 +0900

    Slim anim

--- Assets/01_Scripts/02_Monster/MonsterAtk.cs
+++ Assets/01_Scripts/02_Monster/MonsterAtk.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SearchPlayer : MonoBehaviour
+public class MonsterAtk : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/01_Scripts/02_Monster/MonsterTakeDamage.cs
+++ Assets/01_Scripts/02_Monster/MonsterTakeDamage.cs
@@ -9,23 +9,28 @@ public class MonsterTakeDamage : MonoBehaviour
     [SerializeField]
     SpriteRenderer spriteRenderer;
     [SerializeField]
-    float monster1Health = 100;   
+    float monster1Health;//30
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet1"))
         {
             StartCoroutine(TakeDamage(1));
         }
+        
     }
     IEnumerator TakeDamage(float count)
     {
         monster1Health--;
 
-        animator.SetTrigger("isBlueSlimHurt");
+        animator.SetTrigger("isMonsterHurt");
+
+        if (monster1Health <= 0)
+        {
+            animator.SetTrigger("isMonsterDeath");
 
 
-        yield return null;
-        //yield return new WaitForSecondsRealtime(0.05f);//수정필요
-        //animator.SetBool("isBlueSlimHurt", false);
-    }
+            yield return new WaitForSeconds(1f);//수정필요
+            Destroy(gameObject);
+        }
+    }   
 }
